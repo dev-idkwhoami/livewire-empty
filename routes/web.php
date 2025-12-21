6b35cdb4fabaa992ee21533fbd0aa13767aec0a9
Maximilian Oswald <maxi.osw@outlook.com>
2025-12-21 19:46:44 +0100

    updating starter kit source code to latest version

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
+use App\Livewire\Settings\TwoFactor;
 use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Features;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,9 +18,18 @@ Route::view('dashboard', 'dashboard')
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
 
-    Route::get('settings/profile', Profile::class)->name('settings.profile');
-    Route::get('settings/password', Password::class)->name('settings.password');
-    Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
-});
+    Route::get('settings/profile', Profile::class)->name('profile.edit');
+    Route::get('settings/password', Password::class)->name('user-password.edit');
+    Route::get('settings/appearance', Appearance::class)->name('appearance.edit');
 
-require __DIR__.'/auth.php';
+    Route::get('settings/two-factor', TwoFactor::class)
+        ->middleware(
+            when(
+                Features::canManageTwoFactorAuthentication()
+                    && Features::optionEnabled(Features::twoFactorAuthentication(), 'confirmPassword'),
+                ['password.confirm'],
+                [],
+            ),
+        )
+        ->name('two-factor.show');
+});
